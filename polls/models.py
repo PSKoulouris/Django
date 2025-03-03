8fb2da55284d492d17b83def2793919c77c545bc
PSKoulouris <philippe.koulouris@gmail.com>
2025-03-03 17:05:37 +0100

    poll database

---
+++ polls/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=300)
+    pub_date = models.DateTimeField("date published")
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=300)
+    vote = models.IntegerField(default=0)
+
